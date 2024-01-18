2ab2daeafb87433ddabb7e5e8cb66b9e5a2c510a
benarivo66 <iz4comed@yahoo.com>
2024-01-18 13:28:12 +0100

    Abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Devops Engineer";
+        job2._company = "AWS";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Anthony Izekor";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
+        
+        
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,93 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+
+        Journal journal = new Journal();
+
+        List<string> prompts = new List<string>{
+            "Who was the most interesting person I interacted with today?",
+            "What was the best part of my day?",
+            "How did I see the hand of the Lord in my life today?",
+            "What was the strongest emotion I felt today?",
+            "If I could change one thing today, what would it be?",
+            "What did I do to bless others today?",
+            "How did I draw closer to my goals today?"
+        };
+
+        Console.WriteLine("Welcome to the Journal Program\nPlease select one of the following choices:");
+        Console.Write("1. Write\n");
+        Console.Write("2. Display\n");
+        Console.Write("3. Load\n");
+        Console.Write("4. Save\n");
+        Console.Write("5. Exit\n");
+        string strMenuResp = Console.ReadLine();
+        int menuResp = 0;
+        if (int.TryParse(strMenuResp, out int parsedMenuResp))
+        {
+            menuResp = parsedMenuResp;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid menu option.");
+        }
+        while (menuResp != 5)
+        {
+            if (menuResp == 1)
+            {   
+                Entry entry = new Entry();
+                PromptGenerator promptGen = new PromptGenerator();
+                promptGen._prompts = prompts;
+                string randomPrompt = promptGen.GetRandomPrompt();
+                Console.WriteLine(randomPrompt);
+                Console.Write("> ");
+                string resp = Console.ReadLine();
+
+                entry._date = dateText;
+                entry._promptText = randomPrompt;
+                entry._entryText = resp;
+
+                journal.AddEntry(entry);
+            }
+            else if (menuResp == 2)
+            {
+                    journal.DisplayAll();
+                }
+            else if (menuResp == 3)
+            {
+                Console.WriteLine("What is the file name?");
+                Console.Write("> ");
+                string fileName = Console.ReadLine();
+                journal.LoadFromFile(fileName);
+            }
+            else if (menuResp == 4)
+            {
+                Console.WriteLine("What is the file name?");
+                Console.Write("> ");
+                string fileName = Console.ReadLine();
+                journal.SaveToFile(fileName);
+            }
+            else
+            {
+                Console.WriteLine("Enter a number from one of the displayed numbers in the prompt");
+            }
+            Console.WriteLine("Welcome to the Journal Program\nPlease select one of the following choices:");
+            Console.Write("1. Write\n");
+            Console.Write("2. Display\n");
+            Console.Write("3. Load\n");
+            Console.Write("4. Save\n");
+            Console.Write("5. Exit\n");
+
+            strMenuResp = Console.ReadLine();
+            if (int.TryParse(strMenuResp, out int parsedMenuRes))
+            {
+                menuResp = parsedMenuRes;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid menu option.");
+            }
+        }
     }
 }
